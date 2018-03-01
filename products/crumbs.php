a038a449d22070d87bb628120657d0afa9dddb6d
Kiril Zhelyazkov <knz140@aubg.edu>
2018-03-01 23:59:26 +0200

    keeping track of the path wirh breadcrumbs

--- products/crumbs.php
+++ products/crumbs.php
@@ -1,7 +1,11 @@
 <?php
 $crumbs = explode("/", $_SERVER["REQUEST_URI"]);
 
-foreach ($crumbs as $crumb) {
+echo '<nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>';
+foreach (array_slice($crumbs, 1) as $crumb) {
     #echo ucfirst(str_replace(array(".php","_"), array(""," "), $crumb) . ' ');
-    echo $crumb;
-}
+    echo '<li class="breadcrumb-item"><a href="#">' . ucfirst(str_replace(array(".php", "_"), array(""," "), $crumb)) . '</a></li>';
+  }
+echo '</ol></nav>';
