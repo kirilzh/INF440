a4d5248bdca11a121ac5b8e129e1a0fab2579d06
Hristina Boyadzhieva <hristinaboyadzhieva@gmail.com>
2018-04-19 20:03:53 +0300

    cart

--- cart.php
+++ cart.php
@@ -14,50 +14,16 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $quantity = $_POST['quantity'] + $_SESSION['products'][$_POST['isbn']]['qty'] ;
     $_SESSION['products'][$_POST['isbn']] = array('qty'=>$quantity, 'name'=> $product['name'], 'price'=>$product['price'],'img'=>$product['img']);
 }
-
+ echo json_encode($_SESSION['products']);
 ?>
 
 <html>
 <head>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<!-- Bootstrap core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Custom styles for this template -->
-<link href="css/shop-homepage.css" rel="stylesheet">
-
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 </head>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#"><img src="./public/images/logo.png" style="height:30px;width:30px;margin:0px;"> </a>
-        <a class="navbar-brand" href="#"> The Book Basement </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a id="home_nav" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-              <a id="products_nav" class="nav-link">Products</a>
-            </li>
-            <li class="nav-item">
-              <a img="./images/cart.png" href = "#"></a>
-              <a class="nav-link" href="#">
-                <img  src="./public/images/cart.png" height="20px" width="20px"> Cart</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                  <img  src="./public/images/account.png" height="20px" width="20px"> Your Account</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <body>
+<body>
+
 <!------ Include the above in your HEAD tag ---------->
 
 <div class="container">
@@ -116,6 +82,8 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 					</tfoot>
 				</table>
 </div>
+</body>
+</html>
 <!-- <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 <script src="vendor/jquery/jquery.min.js"></script>
@@ -123,49 +91,44 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 <!-- <script src="vendor/jquery/cart.js"></script> -->
 <script>
     $('.refresh').click(function(){
-        var refresh_id = this.name;
-        var refresh_quant = $('#quant'+refresh_id).val();
-        var refresh_price = $('#price'+refresh_id).text();
-        refresh_price = refresh_price.substring(1,refresh_price.length);
-        console.log(refresh_price);
-        var sess = <?php echo json_encode($_SESSION['products']) ?>;
-        console.log(refresh_id);
-        console.log(refresh_quant);
+    var refresh_id = this.name;
+    var refresh_quant = $('#quant'+refresh_id).val();
+    var refresh_price = $('#price'+refresh_id).text();
+    refresh_price = refresh_price.substring(1,refresh_price.length);
+    var sess = <?php echo json_encode($_SESSION['products']) ?>;
 
-        $('#quant'+refresh_id).html(refresh_quant);
-        $('#subtotal'+refresh_id).html('$'+refresh_quant*refresh_price);
+    $('#quant'+refresh_id).html(refresh_quant);
+    $('#subtotal'+refresh_id).html('$'+refresh_quant*refresh_price);
 
-        var total = 0;
-        var subtotal = 0;
-        for(i = 0; i < 10; i++){
-            console.log($('#subtotal'+i).text());
-            if($('#subtotal'+i).text().match(/\d+/) != null){
-                subtotal = ($('#subtotal'+i).text()).match(/\d+/);
-                total = parseFloat(total) + parseFloat(subtotal[0]);
-            }
+    var total = 0;
+    var subtotal = 0;
+    for(i = 0; i < 10; i++){
+        console.log($('#subtotal'+i).text());
+        if($('#subtotal'+i).text().match(/\d+/) != null){
+            subtotal = ($('#subtotal'+i).text()).match(/\d+/);
+            total = parseFloat(total) + parseFloat(subtotal[0]);
         }
-        $('#total').html('Total $'+total);
-        
-    });
-
-    $('.deletebtn').click(function(){
-        var delete_id = this.name;
-        console.log(delete_id);
-        $('#div'+delete_id).remove();
-
-        var total = 0;
-        var subtotal = 0;
-        for(i = 0; i < 10; i++){
-            console.log($('#subtotal'+i).text());
-            if($('#subtotal'+i).text().match(/\d+/) != null){
-                subtotal = ($('#subtotal'+i).text()).match(/\d+/);
-                total = parseFloat(total) + parseFloat(subtotal[0]);
-            }
-        }
-        $('#total').html('Total $'+total);
-    });
+    }
+    $('#total').html('Total $'+total);
     
+});
+
+$('.deletebtn').click(function(){
+    var delete_id = this.name;
+    console.log(delete_id);
+    $('#div'+delete_id).remove();
+
+    var total = 0;
+    var subtotal = 0;
+    for(i = 0; i < 10; i++){
+        console.log($('#subtotal'+i).text());
+        if($('#subtotal'+i).text().match(/\d+/) != null){
+            subtotal = ($('#subtotal'+i).text()).match(/\d+/);
+            total = parseFloat(total) + parseFloat(subtotal[0]);
+        }
+    }
+    $('#total').html('Total $'+total);
+});
 </script>
 
-</body>
-</html>
+
